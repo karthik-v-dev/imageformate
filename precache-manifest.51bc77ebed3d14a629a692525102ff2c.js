643538159aaaaa7edc9d3657cb0fd878118c560d
karthik-v-dev <karthikvoorugonda@gmail.com>
2020-10-18 15:05:25 +0530

    Updates

---
+++ precache-manifest.51bc77ebed3d14a629a692525102ff2c.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecc8a2f781eab096f00566fba6a6fbc9",
+    "url": "/imageformate/index.html"
+  },
+  {
+    "revision": "baff56dfc96da9d85365",
+    "url": "/imageformate/static/css/main.0a911cdc.chunk.css"
+  },
+  {
+    "revision": "35669f93212099eac918",
+    "url": "/imageformate/static/js/2.f5036b0f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/imageformate/static/js/2.f5036b0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baff56dfc96da9d85365",
+    "url": "/imageformate/static/js/main.d82dfbbe.chunk.js"
+  },
+  {
+    "revision": "2582167ee61e88795ba7",
+    "url": "/imageformate/static/js/runtime-main.af385dd0.js"
+  },
+  {
+    "revision": "e01babd490d4ae1e7f4fc0bd237a20da",
+    "url": "/imageformate/static/media/Beach1.e01babd4.jpg"
+  },
+  {
+    "revision": "83bc3619c5be51e295102abd12aeaabf",
+    "url": "/imageformate/static/media/Beach2.83bc3619.jpg"
+  },
+  {
+    "revision": "260ce0690314598d5e26a3bba9f04ac1",
+    "url": "/imageformate/static/media/Beach3.260ce069.jpg"
+  },
+  {
+    "revision": "9fcfe7638094b53f116d792ea1f11ec8",
+    "url": "/imageformate/static/media/Beach4.9fcfe763.jpg"
+  },
+  {
+    "revision": "6745d32587479a42759ab9335d2695ec",
+    "url": "/imageformate/static/media/Beach5.6745d325.jpg"
+  },
+  {
+    "revision": "dd9cf571f77c01fcc7f5e816a0f9e779",
+    "url": "/imageformate/static/media/Beach6.dd9cf571.jpg"
+  },
+  {
+    "revision": "7fca67ad9e266e7c229bed48c65edc46",
+    "url": "/imageformate/static/media/Bird2.7fca67ad.jpg"
+  },
+  {
+    "revision": "7c31904c79765ab97211a1b0353fd6d8",
+    "url": "/imageformate/static/media/Bird4.7c31904c.jpg"
+  },
+  {
+    "revision": "e3fdb26347c725869d8634180d4dab97",
+    "url": "/imageformate/static/media/Bird5.e3fdb263.jpg"
+  },
+  {
+    "revision": "1401fab5555cd24accdeb01943d9499a",
+    "url": "/imageformate/static/media/Bird6.1401fab5.jpg"
+  },
+  {
+    "revision": "16ddf1a4a1e2871b490a3b295cfbf331",
+    "url": "/imageformate/static/media/Bird7.16ddf1a4.jpg"
+  },
+  {
+    "revision": "01f2658341b5b15b430766d4464c59d9",
+    "url": "/imageformate/static/media/Bird8.01f26583.jpg"
+  },
+  {
+    "revision": "823bbb30596be70dca257dfe92f31a36",
+    "url": "/imageformate/static/media/Food1.823bbb30.jpg"
+  },
+  {
+    "revision": "cd08a88acb87917093a0400ba76d2084",
+    "url": "/imageformate/static/media/Food2.cd08a88a.jpg"
+  },
+  {
+    "revision": "18796dd5ef9b12df1248a41c6a4f05ef",
+    "url": "/imageformate/static/media/Food3.18796dd5.jpg"
+  },
+  {
+    "revision": "35bc12cea34bdce26e96a9d6a3a7de39",
+    "url": "/imageformate/static/media/Food4.35bc12ce.jpg"
+  },
+  {
+    "revision": "e8e4ca71bb2b1ae9c620b7262f443c57",
+    "url": "/imageformate/static/media/Food5.e8e4ca71.jpg"
+  },
+  {
+    "revision": "0540884bffbeefcec38eebec49192320",
+    "url": "/imageformate/static/media/Food6.0540884b.jpg"
+  },
+  {
+    "revision": "fc133f01d3ed63cf21c0879f10d96e39",
+    "url": "/imageformate/static/media/Food7.fc133f01.jpg"
+  },
+  {
+    "revision": "f468f2ad02c65a6503d3c27fad523f0d",
+    "url": "/imageformate/static/media/mountain1.f468f2ad.jpg"
+  },
+  {
+    "revision": "a9fc68f321f608c9a9a7e8f0ee8c0b05",
+    "url": "/imageformate/static/media/mountain2.a9fc68f3.jpg"
+  },
+  {
+    "revision": "8fad0322c8c4ca92d77b433e1c768257",
+    "url": "/imageformate/static/media/mountain3.8fad0322.jpg"
+  },
+  {
+    "revision": "c3ecfacd6fa64722451597081423e0cf",
+    "url": "/imageformate/static/media/mountain4.c3ecfacd.jpg"
+  },
+  {
+    "revision": "cc5f35a61ccd72b37c4ad720a45ef252",
+    "url": "/imageformate/static/media/mountain5.cc5f35a6.jpg"
+  },
+  {
+    "revision": "24405f2e73db935d1937909e5a614aa3",
+    "url": "/imageformate/static/media/mountain6.24405f2e.jpg"
+  },
+  {
+    "revision": "577215bcbd09ebf392b2a9519708e3c0",
+    "url": "/imageformate/static/media/mountain8.577215bc.jpg"
+  }
+]);
